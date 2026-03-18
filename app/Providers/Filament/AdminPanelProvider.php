c8f1784e2883b3efffcadb95f46dd3fb05373b26
Abdelkarim Mateos Sanchez <abdelkarim.mateos@castris.com>
2026-03-18 09:04:21 +0100

    Pre-upgrade stopped by Lody and LR10 decissions

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -8,7 +8,7 @@ use Filament\Pages\Dashboard;
 use Filament\{Panel, PanelProvider, Widgets};
 use Filament\Support\Colors\Color;
 use Illuminate\Cookie\Middleware\{AddQueuedCookiesToResponse, EncryptCookies};
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Foundation\Http\Middleware\PreventRequestForgery;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\{AuthenticateSession, StartSession};
 use Illuminate\View\Middleware\ShareErrorsFromSession;
@@ -41,7 +41,7 @@ class AdminPanelProvider extends PanelProvider
                 StartSession::class,
                 AuthenticateSession::class,
                 ShareErrorsFromSession::class,
-                VerifyCsrfToken::class,
+                PreventRequestForgery::class,
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
